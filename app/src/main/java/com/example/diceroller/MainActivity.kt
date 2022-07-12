0b36c32c71653291755fd500e6db9ddb6df2eb42
lifeforme <siyaga123@gmail.com>
2022-07-12 10:26:24 +0700

    Membuat Roller

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton : Button = findViewById(R.id.btn_roll)
+
+        rollButton.setOnClickListener{rollDice()}
+
+    }
+    private fun rollDice(){
+//        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        val randomInt = (1..6).random()
+
+        val resultText : TextView = findViewById(R.id.tv_roll)
+        resultText.text = randomInt.toString()
+    }
+}
